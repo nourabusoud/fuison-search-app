4f1fcf71cb0e3cb5f531e15ef48cf6280237f9c1
nourabusoud <nour.abusoud@gmail.com>
2018-05-20 23:00:08 -0400

    added elements to sessionView component

--- src/components/sessionSummary.vue
+++ src/components/sessionSummary.vue
@@ -1,11 +1,18 @@
 <template>
-  <section>
+  <section class="item">
     <h2 class="item-title">{{ session.title }}</h2>
     <span class="item-location">{{ session.location }}</span><span class="item-year">{{ session.year }}</span>
     <span class="item-organization">{{ session.organization }}</span>
     <span class="item-speaker">{{ session.speaker }}</span>
+    <input type="checkbox" class="details-toggle">
     <div class="details">
       <p>{{ session.summary }}</p>
+      <div v-if="youtubeLink">
+        <iframe width="420" height="315"  v-bind:src="youtubeLink" frameborder="0" allowfullscreen></iframe>
+      </div>
+      <div v-if="session.slideshare_url">
+        <a v-bind:href="session.slideshare_url">Go to presentation</a>
+      </div>  
     </div>
   </section>
 </template>
@@ -17,7 +24,17 @@ export default {
   data () {
     return {}
   },
-  computed: {},
+  computed: {
+    youtubeLink () {
+      if(this.session.youtube_url !== ''){
+        let link = this.session.youtube_url.split('https://youtu.be/');
+        return `https://www.youtube.com/embed/${link[1]}`
+      } else {
+        return false
+      }
+
+    }
+  },
   mounted () {},
   methods: {}
 }
